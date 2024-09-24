0abadd87f84e9064614038d3e3b1f7424075fe37
AmeyHengle <ameyhengle22@gmail.com>
2024-09-24 16:56:14 +0530

    add custom inference

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 # Replace this with your actual model code
 import time
 import json
-from inference import generate_counternarrative, generate_hs_explanations
+from projects.hate_speech_demo.inference_api import generate_counternarrative, generate_hs_explanations
 from semantic_similarity import find_most_similar_fact
+from inference_custom import CustomLLMInference
+model = CustomLLMInference('t5-small')
 
-# Dummy function to analyze the hate speech statement
 def analyze_statement(statement, return_dummy_values=False):
-    # Toy values for the nature, group, and bias analysis
-    # time.sleep(5)
+    time.sleep(1)
     if return_dummy_values:
      return {
         "nature": "dummy value for nature",
@@ -34,5 +34,7 @@ def generate_counterspeech(statement, inference_type="api", return_dummy_values=
         fact = fact['fact']
         response  = generate_counternarrative(hatespeech=statement, counter_fact=fact)
         return response, f"{fact}\n(Retrieval Confidence: {similarity_score*100}%)"
+    elif inference_type=="custom":
+       response = model.predict(hatespeech=statement, counter_fact=fact)
     
     return f"The statement is inappropriate and perpetuates harmful gender stereotypes. We should respect individuals regardless of their gender and value their contributions equally."
